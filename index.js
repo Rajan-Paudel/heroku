9bb7a1307404995316f1b55d4e0cf8edd439c8e9
Rajan <rajanpdl89@gmail.com>
2021-08-28 14:23:35 +0530

    commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const admin =  require('firebase-admin')
+const express = require('express')
+const app = express()
+
+var serviceAccount = require('./uicapp-78084-firebase-adminsdk-d4ys8-26ea2cca83.json');
+app.use(express.json())
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+app.post('/send-noti',(req,res,)=>{
+    console.log(req.body)
+   const message = {
+    notification:{
+        title: "new notification",
+        body: "title here"
+    },
+    tokens:req.body.tokens
+}
+
+admin.messaging().sendMulticast(message).then(res=>{
+   console.log('send success')
+}).catch(err=>{
+    console.log(err)
+}) 
+})
+
+
